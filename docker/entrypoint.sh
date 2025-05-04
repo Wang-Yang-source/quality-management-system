4f4ae94f8ead2bcc8e949846145c74a6bac7d711
Waya <20234832@stu.cqu.edu.cn>
2025-05-05 05:43:55 +0800

    Update nginx.conf

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-# 替换 Nginx 配置文件中的环境变量
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
-mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
+# 检查是否存在PORT环境变量，如果存在则修改nginx配置
+if [ ! -z "$PORT" ]; then
+  # 将Nginx配置文件中的80端口替换为环境变量指定的端口
+  sed -i.bak "s|listen 80;|listen $PORT;|g" /etc/nginx/conf.d/default.conf
+  echo "Nginx 配置已更新，监听端口: $PORT"
+else
+  echo "未设置PORT环境变量，使用默认端口80"
+fi
 
 # 启动 Nginx
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
